4ec8a536fc6a71f8c3d2ee1b5865d550cd89dd0e
jungpaeng <meis1541@naver.com>
2019-04-25 12:26:12 +0900

    :sparkles: Show Editor Value

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import * as React from 'react';
+import styled from 'styled-components';
 import { Editor } from '@atlaskit/editor-core';
+import { JSONTransformer } from '@atlaskit/editor-json-transformer';
 
-const App = () => (
-  <div>
-    <Editor
-      appearance="comment"
-    />
-  </div>
-);
+const jsonPretty = (obj: any) => JSON.stringify(obj, null, 2);
+
+const PreWrapDiv = styled.div`
+  white-space: pre-wrap;
+
+  &:empty:not(:focus)::before {
+    content: attr(data-placeholder);
+    font-size: 14px;
+  }
+`;
+
+class App extends React.Component {
+  state = {
+    output: ''
+  };
+  transformer = new JSONTransformer();
+
+  handleChangeInEditor = (editorView) => {
+    const output = jsonPretty(this.transformer.encode(editorView.state.doc));
+
+    this.setState({output});
+  };
+
+  render() {
+    return (
+      <>
+        <Editor
+          appearance="comment"
+          onChange={this.handleChangeInEditor}
+        />
+        <PreWrapDiv data-placeholder="Empty Document">
+          {this.state.output}
+        </PreWrapDiv>
+      </>
+    );
+  }
+}
 
 export default App;
