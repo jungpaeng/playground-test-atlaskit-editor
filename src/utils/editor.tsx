012aadeb5a1359f9e0021ebfd7e344d17561da03
jungpaeng <meis1541@naver.com>
2019-04-26 18:53:24 +0900

    :sparkles: Create Editor Utils to Menu Item

---
+++ src/utils/editor.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { InsertMenuCustomItem, ReactComponents } from '@atlaskit/editor-core/types';
+import CustomSVG from '../components/common/CustomSVG';
+import { testIcon } from '../constants/svg';
+import { camelize } from './string';
+
+interface EditorMenuItems {
+  content: string;
+  elemBefore?: ReactComponents;
+  onClick?: () => void;
+}
+
+export const createEditorMenuItem = ({
+  content,
+  elemBefore,
+  onClick
+}: EditorMenuItems): InsertMenuCustomItem => ({
+  content,
+  value: {
+    name: camelize(content)
+  },
+  tooltipDescription: content,
+  tooltipPosition: 'right',
+  elemBefore: (
+    elemBefore
+    || <CustomSVG width="24" height="24" d={testIcon} />
+  ),
+  onClick
+});
